d24417c052ccb20e6aec69a375c34101e37dbc65
Addam Reis <addamreis@gmail.com>
2020-08-10 19:19:36 -0300

    typeof, tofixed, convert to binary

--- 1º exemplos.js
+++ 1º exemplos.js
@@ -1,1 +1,11 @@
-console.log('teste')
+//typeof
+let a = 'teste'
+console.log('type: ' + typeof a + ' ||' + ' text: ' + a)
+
+//casas decimais
+const val1 = 10 * 1.7 / 0.72 + 3;
+console.log(val1);
+console.log(val1.toFixed(3))
+
+//convert to binary
+console.log(val1.toString(2))
