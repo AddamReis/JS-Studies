c4223e7f9efd8b8e207880b012513866b0d46025
Addam Reis <addamreis@gmail.com>
2020-08-11 19:08:29 -0300

    Destructuring Part 1

--- 1º exemplos.js
+++ 1º exemplos.js
@@ -12,7 +12,7 @@ console.log(val1.toString(2))
 */
 
 //Math func
-const raio = 5.6
+/*const raio = 5.6
 const area = Math.PI * Math.pow(raio, 2)
 console.log(area)
 console.log(typeof Math)
@@ -33,9 +33,28 @@ console.log(prod2)
 //Hoisting
 console.log('a = ', a)
 var a = 2
-console.log('a = ', a)
+console.log('a = ', a)*/
 
 /*Cannot access 'b' before initialization*/ 
-console.log('b =', b)
+/*console.log('b =', b)
 let b = 2 
-console.log('b =', b)
+console.log('b =', b)*/
+
+//Destructuring
+const pessoa = {
+    nome: 'Ana',
+    idade: 20,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const {nome, idade} = pessoa
+console.log(nome, idade)
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {endereco: {logradouro, numero, cep}} = pessoa
+console.log(logradouro, numero, cep)
