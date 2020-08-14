77826847c53ba1c7f53e5bc17735a090f5e8c0c9
Addam Reis <addamreis@gmail.com>
2020-08-14 18:19:59 -0300

    Arguments For In and Standard Value

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 //function js é First-Class Object (Citizens)
 //Higher-order function
-
+/*
 //create function
 function fun1() { }
 
@@ -33,3 +33,31 @@ soma(7, 3)(1)
 //OR
 const val = soma(7,3)
 val(1)
+
+*/
+
+//Arguments for in
+function soma(){
+    let soma = 0
+    for(i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+console.log(soma())
+console.log(soma(1.1, 2.2, 3.3))
+
+//standard value (defuze)
+function somax(a, b, c){
+    a = a != undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c // <--- best use
+    return a + b + c
+}
+console.log(somax(5, 8, 6), somax(0, 0, 0))
+
+//standard value (ES6)
+function somay(a =1, b = 1, c =1 ){
+    return a + b + c
+}
+console.log(somay(10, 12, 14), somay(0, 0, 0))
