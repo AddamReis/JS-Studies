91bccbb0569106d17ddaf95470f4f7d9614b2fcb
Addam Reis <addamreis@gmail.com>
2020-08-16 13:01:59 -0300

    Callbacks

--- functions.js
+++ functions.js
@@ -106,7 +106,7 @@ console.log(ola())
 
 //Anonymous Func
 
-const soma = function (x, y) {
+/*const soma = function (x, y) {
     return x + y
 }
 const imprimirResultado = function (a, b, operacao = soma){
@@ -125,4 +125,35 @@ const pessoa = {
         console.log('Opa')
     }
 }
-pessoa.falar()
+pessoa.falar()*/
+
+//Callback
+
+const fabricantes = ["GM","Audi","Renault"]
+function imprimir(nome, indice){
+console.log(`${indice + 1}. ${nome}`)
+}
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
+
+
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+//not callback
+const notasbaixas1 = []
+for (let i in notas){
+    if(notas[i] < 7 ){
+        notasbaixas1.push(notas[i])
+    }
+}
+console.log(notasbaixas1)
+
+//in callback
+const notasbaixas2 = notas.filter(function (nota) {
+    return nota < 7
+})
+console.log(notasbaixas2)
+
+//Arrow Callback
+const notasbaixas3 = notas.filter(nota => nota < 7)
+console.log(notasbaixas3)
