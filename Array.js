09668e1a98e58c6499bb868ba5968025af707ee5
Addam Reis <addamreis@gmail.com>
2020-08-25 20:15:45 -0300

    Map Part 1

--- Array.js
+++ Array.js
@@ -38,7 +38,7 @@ aprovados.forEach(nome => console.log(nome)) //Arrow Apenas com 1 parametro
 const exibirAprovados = aprovado => console.log(aprovado) //passando a funcao para uma const
 aprovados.forEach(exibirAprovados) //executando foreach na const
 */
-
+/*
 Array.prototype.forEach2 = function(callback) {
     for (let i = 0; i < this.length; i++) {
         callback(this[i], i, this) //Armazenado a callback dentro do prototype de Array
@@ -50,3 +50,22 @@ const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 aprovados.forEach2(function(nome, indice) {  //executando os índices do array
     console.log(`${indice + 1}) ${nome}`)
 })
+*/
+
+//Map pode ser utilizado para mapear os dados de um array, gerar outro array com os dados transformados.
+//O array gerado através do Map terá o mesmo tamanho do array original
+const nums = [1, 2, 3, 4, 5]
+
+// For com propósito
+let resultado = nums.map(function(e) { //Por padrão, (e) se refere ao elemento do array
+    return e * 2                       //percorrendo cada item do array, multiplicando por 2 e salvando em outro array
+})
+
+console.log(resultado)
+
+const soma10 = e => e + 10 //arrow por  padrão tem o return embutido, não precisa da declaração para retornar
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro) //chamada de métodos em sequence para o mesmo array //para cada map foi gerado um novo array
+console.log(resultado)
