0286d08a56d1def39e03e72e0bbded31328a267a
Addam Reis <addamreis@gmail.com>
2020-08-25 20:04:44 -0300

    Foreach Part 2

--- Array.js
+++ Array.js
@@ -26,10 +26,10 @@ console.log(algunsPilotos1)
 
 const algunsPilotos2 = pilotos.slice(1, 4)
 console.log(algunsPilotos2)*/
-
+/*
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
-aprovados.forEach(function(nome, indice) /*callback com 2 parametros*/ {
+aprovados.forEach(function(nome, indice) { //callback com 2 parametros
     console.log(`${indice + 1}) ${nome}`)
 })
 
@@ -37,3 +37,16 @@ aprovados.forEach(nome => console.log(nome)) //Arrow Apenas com 1 parametro
 
 const exibirAprovados = aprovado => console.log(aprovado) //passando a funcao para uma const
 aprovados.forEach(exibirAprovados) //executando foreach na const
+*/
+
+Array.prototype.forEach2 = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this) //Armazenado a callback dentro do prototype de Array
+    }
+}
+
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach2(function(nome, indice) {  //executando os índices do array
+    console.log(`${indice + 1}) ${nome}`)
+})
