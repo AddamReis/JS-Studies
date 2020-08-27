103f4517d2b43912941cd7e22affd2c9529879f6
Addam Reis <addamreis@gmail.com>
2020-08-26 21:52:45 -0300

    Filter Part 2

--- Array.js
+++ Array.js
@@ -108,7 +108,7 @@ console.log(resultado) //O retorno será o mesmo, porém é uma segunda forma de
 
 //Filter é utilizado para filtrar os dados dentro do array
 
-const produtos = [
+/*const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
     { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
@@ -122,4 +122,27 @@ console.log(produtos.filter(function(p) {
 const caro = produto => produto.preco >= 500 //const que contém função 
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil)) //passado as duas funcoes, tanto p/ valores acima de 500 quanto p/ fragil == true
+console.log(produtos.filter(caro).filter(fragil)) //passado as duas funcoes, tanto p/ valores acima de 500 quanto p/ fragil == true*/
+
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if(callback(this[i], i, this)) { //elemento, índice e array
+            newArray.push(this[i])       //
+        }
+    }
+    return newArray
+}
+
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter2(caro).filter2(fragil))
+
