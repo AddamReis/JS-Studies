060788297ea860ba794d5cebcffe82529155ba33
Addam Reis <addamreis@gmail.com>
2020-08-28 19:33:12 -0300

    Modules

---
+++ Node/ClientModule.js
@@ -0,0 +1,13 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
+
+//Por pardrão,  cada arquivo (Módulo) é privado, para que outros módulos consigam acessa-lo, é necessário exportar e importar.
