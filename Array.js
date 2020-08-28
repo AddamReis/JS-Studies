06e1d09db37431c48e87d97d4e437ff86f7598d1
Addam Reis <addamreis@gmail.com>
2020-08-28 19:05:04 -0300

    FlatMap

--- Array.js
+++ Array.js
@@ -218,10 +218,46 @@ const soma = (total, atual) => total + atual
 
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)*/
-
+/*
 const filhas = ['Ualeskah', 'Cibalena']
 const filhos = ['Uoxiton', 'Uesclei']
 const todos = filhas.concat(filhos)
 console.log(todos, filhas, filhos)
 
-console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))*/
+
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}] //array de objetos
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+
+console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ])) //retorno do flatmap será desta mesma forma
+
+Array.prototype.flatMap = function(callback) {
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
